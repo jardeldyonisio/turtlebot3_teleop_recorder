5bc6f730c6cd3dfc8dd72e017a2102af843c3799
jardeldyonisio <jardel.dyonisio@hotmail.com>
2023-08-08 17:39:09 -0300

    manual waypoints working

--- scripts/bug1_lognav.py
+++ scripts/bug1_lognav.py
@@ -14,9 +14,33 @@ class Navigator:
     def __init__(self):    
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.position_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback, queue_size=10)
-        self.goals = np.array([[0.7, -1.5], [1.5, 2.0], [-0.3, -0.0]])
+        self.goals = np.array([[0.5, -1.2], [1.5, 0.0], [1.0, 2.0]])
+
+    # def read_points_from_file(self, file_path):
+    #     with open(file_path, 'r') as f:
+    #         for line in f:
+    #             x, y = line.strip().split()
+    #             point = PoseStamped()
+    #             point.header.frame_id = "map"
+    #             point.pose.position.x = float(x)
+    #             point.pose.position.y = float(y)
+    #             point.pose.position.z = 0.0
+    #             point.pose.orientation.x = 0.0
+    #             point.pose.orientation.y = 0.0
+    #             point.pose.orientation.z = 0.0
+    #             point.pose.orientation.w = 1.0
+    #             self.goals.append(point)
+    #     return self.goals
 
     def odom_callback(self, data: Odometry):
+        msg = Twist()
+
+        # Se não houver metas definidas, não faça nada
+        if len(self.goals) == 0:
+            msg.angular.z = 0.0
+            msg.linear.x = 0.0
+            return
+
         quaternion = data.pose.pose.orientation 
         quaternion_list = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
         _, _, self.yaw = euler_from_quaternion(quaternion_list)
@@ -34,16 +58,8 @@ class Navigator:
         self.yaw_d = np.arctan2(self.dist_vec[1], self.dist_vec[0])
         self.diff_yaw = self.yaw_d - self.yaw
 
-        msg = Twist()
-
         print("Goals", self.goals)
-        
-        # Se não houver metas definidas, não faça nada
-        if len(self.goals) == 0:
-            msg.angular.z = 0.0
-            msg.linear.x = 0.0
-            return
-
+    
         if self.diff_yaw > self.THRESHOLD_YAW:
             # rospy.loginfo("Rotacionando")
             msg.linear.x = .0
