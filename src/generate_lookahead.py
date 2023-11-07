bc032a84404c115731cae71879f9b83cc28cd9f3
jardeldyonisio <jardel.dyonisio@hotmail.com>
2023-11-07 16:58:19 -0300

    added generate_lookahead file

--- src/generate_lookahead.py
+++ src/generate_lookahead.py
@@ -2,7 +2,11 @@
 
 import rospy
 import numpy as np
+
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32MultiArray
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
 
 LOOKAHEAD_ANGLESPAN = np.pi / 2.0
 LOOKAHEAD_TOTALPATHS = 100
@@ -10,72 +14,67 @@ LOOKAHEAD_DISTBTWPOINTS = 0.3
 LOOKAHEAD_POINTSPERPATH = 20
 LOOKAHEAD_SIMSTEPS = 10
 
-class LookaheadGenerator:
-    def __init__(self):
-        # Inicialize o nó ROS
-        rospy.init_node('lookahead_generator', anonymous=True)
-        self.sub = rospy.Subscriber('/odom', Odometry, self.callback)
-        self.lookahead_ref = dict()
+class lookahead:
 
-        # ... (restante do código de inicialização)
+    def __init__(self):
+        rospy.init_node('lookahead_generator_node')
+        self.pub = rospy.Publisher('lookahead_curves', Float32MultiArray, queue_size=10)
+        self.sub = rospy.Subscriber('/odom', Odometry, self.callback)
+        self.marker_pub = rospy.Publisher('lookahead_curves_markers', Marker, queue_size=10)
+        self.rate = rospy.Rate(50) # 10hz
+        self.points = []
 
     def callback(self, msg : Odometry):
-        self.x = msg.pose.pose.position.x
-        self.y = msg.pose.pose.position.y
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        self.point = Point(x, y, 0)
+
+    def get_state(self):
+        '''
+        Retorna o vetor de estados q
+        '''
+        q = [self.tractor.x, self.tractor.y,
+                self.tractor.steer_angle, self.tractor.angle]
+        for cart in self.train[1:]:
+            q += [cart.angle+cart.steer_angle, cart.angle]
+        q = np.array(q).T
+        return q
 
     def generate_lookahead(self, angle_span=LOOKAHEAD_ANGLESPAN,
                            total_paths=LOOKAHEAD_TOTALPATHS,
                            dist_btw_points=LOOKAHEAD_DISTBTWPOINTS,
                            points_per_path=LOOKAHEAD_POINTSPERPATH,
                            sim_steps=LOOKAHEAD_SIMSTEPS):
-        '''
-        Esta função calcula os pontos referentes a diferentes esterçamentos
-        do rebocador, olhando sempre à frente, para decidir qual esterçamento
-        melhor alinhará com o caminho a ser seguido.
-        '''
-        d = dict()
-        # Cria um trem com zero reboques (temporário)
 
-        # train = tugger.Train(0)
-        # Comentei a linha porque acredito que não vá ser necessário criar o train
-        
-        # Para cada esterçamento
+        d = dict()
+        tyre_radius = 0.2
         dt = 1.0 / sim_steps
         for steering_angle in np.linspace(-angle_span/2.0,
                                           angle_span/2.0, total_paths):
             # Reseta posição do trator e ajusta esterçamento fixo
             u = np.array([0.0, 0.0, steering_angle, 0.0]).T
-            
-            # train.set_state(u)
-            # Precisa setar de alguma maneira o valor de u
-
+            self.set_state(u)
             d[steering_angle] = list()
             # Para cada ponto
             for _ in range(points_per_path):
                 # Grava coordenada atual
-
-                # x, y = train.tractor.get_state()
-                # d[steering_angle].append((x, y))
-                # Comentei porque o a posição X e Y será coletada do odom
-
-                d[steering_angle].append((self.x, self.y))
+                x, y = self.point
+                d[steering_angle].append((x, y))
                 # Avança distância definida
                 for _ in range(sim_steps):
                     step_dist = dist_btw_points/sim_steps
-                    u = np.array([(step_dist / train.tractor.tyre_radius) / dt,
+                    u = np.array([(step_dist / tyre_radius) / dt,
                                   0.0])
-                    q = train.get_state()
-                    # Verificar como gerar esse q
+                    q = self.point
                     j = train.get_jacobian()
-                    # Verificar como gerar o jacobian
                     q_dot = np.dot(j, u)
                     q += q_dot*dt
-                    train.set_state(q)
+                    self.set_state(q)
         self.lookahead_ref = d
 
 if __name__ == '__main__':
     try:
-        generator = LookaheadGenerator()
-        generator.generate_lookahead()
+        lookahead = lookahead()
+        lookahead.generate_lookahead()
     except rospy.ROSInterruptException:
         pass
