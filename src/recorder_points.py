ec8a03352380c49dd948a4c4f22d3d5251b9f668
jardeldyonisio <jardel.dyonisio@hotmail.com>
2023-11-17 13:05:54 -0300

    trying to convert recorder_points to ros2

--- src/recorder_points.py
+++ src/recorder_points.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
 
-import rospy
+import rclpy
+
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
 
 class Recorder:
 
     def __init__(self):
-        rospy.init_node('recorder', anonymous=True)
-        self.sub = rospy.Subscriber('/odom', Odometry, self.callback)
-        self.rate = rospy.Rate(50) # 10hz
+        self.node = rclpy.create_node('recorder')
+        self.sub = self.node.create_subscription(Odometry, '/odom', self.callback, 10)
         self.points = []
         self.file_path = '/home/jardeldyonisio/lognav_ws/src/turtlebot3_teleop_recorder/data/recorded_before.txt'
 
-    def callback(self, msg : Odometry):
+    def callback(self, msg: Odometry):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
         point = Point(x, y, 0)
         self.points.append(point)
 
     def run(self):
-        while not rospy.is_shutdown():
+        while rclpy.ok():
             with open(self.file_path, 'a') as f:
                 while len(self.points) > 0:
                     point = self.points.pop(0)
                     f.write('{},{}\n'.format(point.x, point.y))
-            rospy.loginfo("Points saved")
-            self.rate.sleep()
+            self.node.get_logger().info("Points saved")
 
-if __name__ == '__main__':
+def main(args=None):
+    rclpy.init(args=args)
+
     try:
         recorder = Recorder()
         recorder.run()
-    except rospy.ROSInterruptException:
-        pass
+    finally:
+        recorder.node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
